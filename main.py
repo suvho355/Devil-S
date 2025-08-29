4eae5c9a1b9ecc13db9dcdada99ee7e6ef7ff2f3
suvho355 <suvho355@gmail.com>
2025-08-30 01:37:05 +0530

    main.py

--- main.py
+++ main.py
@@ -79,7 +79,7 @@ def send_comment():
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Vampire RuLex Harshu </title>
+  <title>Shibaji</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
   <style>
     label {
